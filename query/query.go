8cc956ad9b83d581d1ce40f41cfe47c414956553
baorongquan <brq0122@163.com>
2018-08-03 09:49:02 +0800

    update query

--- query/query.go
+++ query/query.go
@@ -8,8 +8,26 @@ import (
 const DATABASE = "test"
 
 func Query(c client.Client) {
-	q := client.NewQuery("SELECT count(busy) FROM cpu_usage", DATABASE, "ns")
-	if response, err := c.Query(q); err == nil && response.Error() == nil {
-		log.Println(response.Results)
+	q := client.NewQuery("SELECT * FROM cpu_usage Limit 5", DATABASE, "ns")
+	response, err := c.Query(q)
+	if err == nil {
+		if response.Error() != nil {
+			log.Println(" err :", response.Error())
+			return
+		}
+	} else {
+		log.Fatalln(err)
+		return
+	}
+
+	res := response.Results
+	for _, r := range res {
+		for _, s := range r.Series {
+			log.Println("tag :", s.Tags)
+			log.Println("columns:", s.Columns)
+			for _, v := range s.Values {
+				log.Println("fields :", v)
+			}
+		}
 	}
 }
